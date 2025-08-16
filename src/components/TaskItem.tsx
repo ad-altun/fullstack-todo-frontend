745faa0bbccb87d395d415d7f2e17c2021f4c2d4
ad-altun <adaltun@web.de>
2025-08-16 10:07:57 +0200

    todo-ui: add icons

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,4 +1,6 @@
 import type {TaskStatus} from "../types/taskTypes.ts";
+import EditNoteRoundedIcon from '@mui/icons-material/EditNoteRounded';
+import PlaylistRemoveRoundedIcon from '@mui/icons-material/PlaylistRemoveRounded';
 
 type ListItemProps = {
     title: string,
@@ -7,20 +9,20 @@ type ListItemProps = {
 }
 
 export default function TaskItem({ title, note, status }: ListItemProps) {
-
+    console.log({status})
     return (
         <div className='tasks'>
             <div className='task-item-content'>
                 <input type="checkbox" name="checkbox" id="checkbox" />
                 <div className='task-item'>
-                    <p>{title}</p>
-                    <p>{note}</p>
-                    <p>{status}</p>
+                    <p className='task-title'>{title}</p>
+                    <p className='task-note'>{note}</p>
+                    {/*<p className='task-text'>{status}</p>*/}
                 </div>
             </div>
             <div className='task-item-buttons'>
-                <button type="button">Edit</button>
-                <button type="button">Delete</button>
+                <EditNoteRoundedIcon />
+                <PlaylistRemoveRoundedIcon />
             </div>
         </div>
     )
